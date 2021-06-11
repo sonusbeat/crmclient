5e7c27ca49106d69795a415a8892a3993a05ec9e
Daniel <sonusbeat@gmail.com>
2021-06-11 08:29:23 -0500

    Login Styles

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,9 +1,59 @@
 import Layout from "../components/Layout";
 
 const Login = () => {
+
+  const inputStyles = "shadow-md appereance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline";
+  const labelStyles = "block text-gray-700 text-sm font-bold mb-2";
+
   return (
     <Layout>
       <h1 className="text-center text-white text-2xl font-light">Login</h1>
+
+      <div className="flex justify-center mt-5">
+        <div className="w-full max-w-sm">
+          <form
+            className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+          >
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className={ labelStyles }
+              >Email</label>
+
+              <input
+                className={ inputStyles }
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="off"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor="password"
+                className={ labelStyles }
+              >Password</label>
+
+              <input
+                className={ inputStyles }
+                id="password"
+                name="password"
+                type="password"
+              />
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="bg-gray-800 w-full p-2 text-white rounded hover:bg-gray-900 uppercase"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </Layout>
   );
 }
