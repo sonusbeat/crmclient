c46afc9f9c91ebb2b751382d70add93fd172b079
Daniel <sonusbeat@gmail.com>
2021-06-11 08:07:00 -0500

    Login and New Account Pages

---
+++ pages/login.jsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const Login = () => {
+  return (
+    <Layout>
+      <h1 className="text-center text-white text-2xl font-light">Login</h1>
+    </Layout>
+  );
+}
+ 
+export default Login;
---
+++ pages/new_account.jsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const NewAccount = () => {
+  return (
+    <Layout>
+      <h1 className="text-center text-white text-2xl font-light">New Account</h1>
+    </Layout>
+  );
+}
+ 
+export default NewAccount;
