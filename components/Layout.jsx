161fbb90f17f0e9322b620c30f145bb6d0e0366c
Daniel <sonusbeat@gmail.com>
2021-06-11 06:58:55 -0500

    Head and Tailwind

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,29 @@
+import Head from "next/head";
+import Sidebar from "../components/Sidebar";
+
 const Layout = ({ children }) => {
   return (
     <>
-      { children }
+      <Head>
+        <title>Clients Admin</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+        <link
+          href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <Sidebar />
+
+      {children}
     </>
   );
-}
+};
 
 export default Layout;
