e8cccd6c2c66b4111e22c12326ab41f4288a02e8
Daniel <sonusbeat@gmail.com>
2021-06-11 09:40:21 -0500

    Installing Apollo Client and DevTools

--- config/apollo.js
+++ config/apollo.js
@@ -2,8 +2,9 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import fetch from "node-fetch";
 
 const client = new ApolloClient({
+  connectToDevTools: true,
   cache: new InMemoryCache(),
-  link: HttpLink({
+  link: new HttpLink({
     uri: "http://localhost:4000/",
     fetch
   }),
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import { ApolloProvider } from "@apollo/client";
+import client from "../config/apollo";
+
 import "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={ client }>
+      <Component { ...pageProps } />
+    </ApolloProvider>
+  );
 };
 
 export default MyApp;
