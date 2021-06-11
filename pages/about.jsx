c5811c8ef2ad6dab5ce91d3ae3b73eb34a014b91
Daniel <sonusbeat@gmail.com>
2021-06-10 19:27:05 -0500

    Initial Setup

---
+++ pages/about.jsx
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout";
+
+const About = () => {
+  return (
+    <>
+      <Layout>
+        <p>This content is children from about us</p>
+      </Layout>
+    </>
+  )
+}
+
+export default About;
---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout";
+
+const Index = () => {
+  return (
+    <>
+      <Layout>
+        <p>This content is children from index</p>
+      </Layout>
+    </>
+  )
+}
+
+export default Index;
